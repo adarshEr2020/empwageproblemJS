2b3300e086f7d21f6fc4b37a34e56c46254f49aa
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-25 16:32:03 +0530

    [Adarsh]Added: calculate empwage

--- EmployeeWageJS/EmpWage.js
+++ EmployeeWageJS/EmpWage.js
@@ -1,1 +1,9 @@
-console.log("Welcome to employee wage problem:");
+const IS_ABSENT = 0;
+
+let empcheck = Math.floor(Math.random() * 10) % 2;
+if(empcheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+}else {
+    console.log("Employee is Present");
+}
